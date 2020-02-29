219cc354d85a74642da995faa46af31f0a6b0a13
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-29 00:29:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 require 'prime'
 
  def prime?(num)
-        (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
+        (2..Math.sqrt(num)).each { |i| return false if num % 1 == 0}
         true
     end
